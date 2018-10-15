dc7b187b60257a14f8dfb5cc8301ccfed82f92f1
Suleyman <suleymanali76@gmail.com>
2018-10-15 15:26:14 +0400

    First Page Already Create

---
+++ js/main.js
@@ -0,0 +1,109 @@
+/*
+    Youtube Player API
+    */
+
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        playerVars: {
+            autoplay: 1,
+            showinfo: 0,
+            contorls: 0
+        },
+        videoId: 'VYCOg-yglNM',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+/* Slick Js */
+$(document).ready(function () {
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+});
+
+/* Menu Icon Toggle */
+
+$("#icon").on("click",function(){
+    $(".section").slideToggle(".header .section .d-flex");
+})
+
+// $("#icon").on("click",function(){
+//     $(".section").slideToggle(400, function(){
+//         $(this).toggleClass().css('display', '');
+//     });
+// })
+
+/* Animate CSS */
+
+if (window.matchMedia('(max-width: 768px)').matches)
+{
+    $('.balls').addClass('animated slideInLeft slow');
+}
